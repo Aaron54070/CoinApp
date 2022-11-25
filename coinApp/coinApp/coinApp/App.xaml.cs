fbe33317c6ad5b8d1ae97e906fbfba9f29d5d858
Aaron54070 <aaronbenjamin54070@gmail.com>
2022-11-25 22:05:54 +1300

    final sub

---
+++ coinApp/coinApp/coinApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace coinApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android={\"819203c6-5adb-4956-a4f7-5c0a7612c16f\"};" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here};" +
+                  "macos={Your macOS App secret here};",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ coinApp/coinApp/coinApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using coinApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using Xamarin.Forms;
+
+namespace coinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private string ApiKey = "F8F8694E-580B-4D7C-A413-AE2150567103";
+        private string baseimageUrl = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_256/";
+        public MainPage()
+        {
+            InitializeComponent();
+            coinListView.ItemsSource = GetCoins();
+        }
+
+        private List<Coin> GetCoins()
+        {
+            List<Coin> coins;
+            var client = new RestClient("http://rest.coinapi.io/v1/assets/?filter_asset_id=BTC;DOGE;SGD;LTC;KRW");
+            var request = new RestRequest();
+            request.AddHeader("X-CoinAPI-Key", ApiKey);
+
+            var response = client.Execute(request);
+            coins = JsonConvert.DeserializeObject<List<Coin>>(response.Content);
+
+            foreach (var c in coins)
+            {
+                if (!string.IsNullOrEmpty(c.Id_icon))
+                    c.Icon_url = baseimageUrl + c.Id_icon.Replace("-", "") + ".png";
+                else
+                    c.Icon_url = "";
+            }
+            return coins;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            coinListView.ItemsSource = GetCoins();
+        }
+    }
+}
